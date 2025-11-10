af42a0dad39e9eee98b999903d259f342d6b9beb
jahui35 <jiahuitan.005@gmail.com>
2025-11-10 16:42:46 +0800

    uodated user class

--- User.java
+++ User.java
@@ -4,21 +4,31 @@ public abstract class User {
 	private String name;
 	private String password;
 	private filterSetting filterSettings;
+	private boolean isLoggedIn = false; 
 
-	//do we need to add constructor 
+	
 	public User(String userID, String name, String password) {
 		this.userID=userID;
 		this.name=name;
-		this.password=password;
+		this.password=password; // passwrod is password by default
 	}
 
-	public Boolean login() {
+	public Boolean login(String in) { //need to update class diagram
 		// TODO - implement User.login
+		if (in == this.password){
+			isLoggedIn = true;
+			System.out.println("You are now logged in.");
+			return true;
+		}
+		System.out.println("Wrong password. Please try again.");
+		
 		throw new UnsupportedOperationException();
 	}
 
 	public void logout() {
 		// TODO - implement User.logout
+		isLoggedIn = false;
+		System.out.println("You are now logged out.");
 		throw new UnsupportedOperationException();
 	}
 
@@ -29,6 +39,13 @@ public abstract class User {
 	 */
 	public Boolean changePassword(String oldPass, String newPass) {
 		// TODO - implement User.changePassword
+		if (this.password == oldPass){
+			this.password = newPass;
+			System.out.println("Password succesfully updated.");
+		}else if (this.password != oldPass){
+			System.out.println("Wrong password. Please  try again.");
+		}
+
 		
 		throw new UnsupportedOperationException();
 	}
