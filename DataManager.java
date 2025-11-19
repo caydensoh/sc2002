91caba8fdb1413219b30a13796e7ac3b4e1ae6d7
Wei Yong <weiyongt203@gmail.com>
2025-11-19 12:58:16 +0800

    Revert "Added interface class to satisfy DIP and added missing file paths"
    This reverts commit 4d4d15a7cc30047f2f22affe2119ca83439e01a1.

--- DataManager.java
+++ DataManager.java
@@ -6,10 +6,8 @@ import java.util.*;
 /**
  * Handles all CSV file I/O operations for users, internships, and applications.
  */
-public class DataManager implements IDataManager{
+public class DataManager {
 	private static final String USERS_CSV_PATH = "test_cases/sample_student_list.csv";
-	private static final String COM_REP_CSV_PATH = "test_cases/company_representatives.csv";
-	private static final String CAR_CEN_STAFF_CSV_PATH = "test_cases/career_center_staff.csv"; 
 	private static final String INTERNSHIPS_CSV_PATH = "test_cases/internships.csv";
 	private static final String APPLICATIONS_CSV_PATH = "test_cases/applications.csv";
     private static final String FILTERS_CSV_PATH = "test_cases/filter_settings.csv";
@@ -17,8 +15,7 @@ public class DataManager implements IDataManager{
 	/**
 	 * Load all data from CSV files
 	 */
-	@Override
-	public void loadAllData(List<User> users, List<Internship> allInternships, List<Application> allApplications) {
+	public static void loadAllData(List<User> users, List<Internship> allInternships, List<Application> allApplications) {
 		loadInternshipsFromCSV(allInternships);
 		loadApplicationsFromCSV(allApplications, allInternships);
 		loadUsersFromCSV(users, allApplications);
@@ -29,8 +26,7 @@ public class DataManager implements IDataManager{
 	/**
 	 * Save all data to CSV files
 	 */
-	@Override
-	public void saveAllData(List<User> users, List<Internship> allInternships, List<Application> allApplications) {
+	public static void saveAllData(List<User> users, List<Internship> allInternships, List<Application> allApplications) {
 		saveUsersToCSV(users);
 		saveInternshipsToCSV(allInternships);
 		saveApplicationsToCSV(allApplications);
